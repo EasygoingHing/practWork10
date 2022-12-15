61e9332e89f148437bbc96e81192269b393f7cda
Мой компьютер <krutolapov98@gmail.com>
2022-12-15 16:49:17 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Пример_таблицы_WPF;
+
+namespace WpfApp10
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<float> expensesArray, //затраты
+                    pricesArray;   //цены          
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                tbResult.Clear();
+                int numberOfProducts = int.Parse(tbСountProducts.Text);
+
+                expensesArray = new List<float>(numberOfProducts);   //затраты на производство
+                pricesArray = new List<float>(numberOfProducts);     //цена продукта
+
+                Random rnd = new Random();
+
+                for (int i = 0; i < numberOfProducts; i++)
+                {
+                    expensesArray.Add(rnd.Next(1000));
+                    pricesArray.Add(rnd.Next(1000));
+
+                    if (expensesArray[i] > pricesArray[i] && tbResult.Text == "")
+                    {
+                        tbResult.Text = (i + 1).ToString();
+                    }
+                }
+                dataGrid.ItemsSource = VisualArray.ToDataTable(expensesArray, pricesArray).DefaultView;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный формат данных");
+            }            
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Крутолапов Валерий Исп-31 В-1");
+        }
+    }
+}
---
+++ VisualArray.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace Пример_таблицы_WPF
+{
+    //Класс для визуализации данных на DataGrid.
+    static class VisualArray
+    {
+        /// <summary>
+        /// Реализует визуализацию даннных на DataGrid из передаваемого списка структуры PcInfo.
+        /// </summary>
+        /// <param name="pcList">Список содержащий экземпляры структуры PcInfo.</param>
+        /// <returns>Представление таблицы.</returns>
+        public static DataTable ToDataTable(List<float> expensesArray, List<float> pricesArray)
+        {
+            var tempDataGrid = new DataTable();
+            DataRow row, rowSecond;//Объявление строки
+
+            for (int i = 0; i < expensesArray.Count; i++)
+            {
+                tempDataGrid.Columns.Add($"Продукт №{i + 1}" , typeof(float));
+            }
+            
+            row = tempDataGrid.NewRow();
+            rowSecond = tempDataGrid.NewRow();//создание строки
+
+            for (int i = 0; i < expensesArray.Count; i++)
+            {
+                row[i] = expensesArray[i];
+                rowSecond[i] = pricesArray[i];
+            }
+
+            tempDataGrid.Rows.Add(row);
+            tempDataGrid.Rows.Add(rowSecond); //добавление в таблицу
+
+            return tempDataGrid;
+        }
+    }
+}
